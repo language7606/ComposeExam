983e154851640a53dd21fd1afe0079a3f40ffb10
language7606 <tjddnjspower@naver.com>
2024-07-01 16:54:46 +0900

    [Chore] Chapter20 컴포즈 상태와 재구성 (rememberSaveable)

--- app/src/main/java/com/example/stateexample/MainActivity.kt
+++ app/src/main/java/com/example/stateexample/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.stateexample.ui.theme.StateExampleTheme
@@ -26,16 +27,16 @@ class MainActivity : ComponentActivity() {
                         DemoScreen()
                         FunctionA()
                     }
-
                 }
             }
         }
+
     }
 }
 
 @Composable
 fun DemoScreen() {
-    var textState by remember { mutableStateOf("") }
+    var textState by rememberSaveable { mutableStateOf("") }
 
     val onTextChange = { text: String ->
         textState = text
